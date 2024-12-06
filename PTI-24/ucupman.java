26d3e135c37b0d78228c8a4aa2f70c33bae075a6
Valentinus Delvin <delvinwicaksono@gmail.com>
2024-12-07 06:01:26 +0700

    Create ucupman.java

---
+++ PTI-24/ucupman.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class upperman {
+    public static void main(String[] args) {
+        Scanner braws = new Scanner(System.in);
+        int lebar = braws.nextInt();
+        int panjang = braws.nextInt();
+        int tebal = braws.nextInt();
+
+        for (int i = 0; i<panjang ; i++){
+            for (int j=0; j<lebar;j++){
+                if (j<tebal||j>=(lebar-tebal)){
+                    System.out.print("U");
+                } else{
+                    if (i>=panjang-tebal){
+                        System.out.print("U");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+        braws.close();
+    }
+}
